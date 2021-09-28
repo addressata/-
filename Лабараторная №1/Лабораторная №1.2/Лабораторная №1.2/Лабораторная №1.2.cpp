4a66a106037ed8e52a5269d2e63eb85431708e7c
addressata <90573281+addressata@users.noreply.github.com>
2021-09-28 19:57:52 +0300

    Add files via upload

---
+++ Лабараторная №1/Лабораторная №1.2/Лабораторная №1.2/Лабораторная №1.2.cpp
@@ -0,0 +1,25 @@
+﻿// Лабораторная №1.2.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <math.h>
+
+int main()
+{
+	
+	int a;
+	a = 6;
+	int b;
+	b = 8;
+	int c;
+	c = 10;
+	int p;
+	p = (a + b + c) / 2;
+	float S;
+	S = sqrt(p * (p - a) * (p - b) * (p - c));
+	
+
+ 
+}
+
+
