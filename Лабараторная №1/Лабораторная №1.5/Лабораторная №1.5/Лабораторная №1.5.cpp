384989d4a7e8bc0702377149566e3fcb73304367
addressata <90573281+addressata@users.noreply.github.com>
2021-09-28 21:04:43 +0300

    Add files via upload

---
+++ Лабараторная №1/Лабораторная №1.5/Лабораторная №1.5/Лабораторная №1.5.cpp
@@ -0,0 +1,16 @@
+﻿// Лабораторная №1.5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+int main()
+{
+    int People;
+    int Computer;
+    People = 10000;
+    Computer = People + 1;
+
+
+
+}
+
