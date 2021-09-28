79ad51f186dfd67b841ffe5dc9598f90c3aa153c
addressata <90573281+addressata@users.noreply.github.com>
2021-09-28 19:32:34 +0300

    Add files via upload

---
+++ Лабараторная №1/Лабараторная №1/Лабараторная №1.cpp
@@ -0,0 +1,29 @@
+﻿
+
+#include <iostream>
+
+int main()
+{
+    int Char;
+    Char = sizeof(char);
+        
+    int Sshort;
+    Sshort = sizeof(short);
+    
+    int Int;
+    Int = sizeof(int);
+
+    int Long;
+    Long = sizeof(long);
+
+    int Float;
+    Float = sizeof(float);
+
+    int Double;
+    Double = sizeof(double);
+
+
+
+
+ 
+}
