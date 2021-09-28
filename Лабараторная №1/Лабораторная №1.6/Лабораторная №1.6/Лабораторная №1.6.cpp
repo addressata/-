60a2319b101197720f8d8e3d7ac33a1f859280cb
addressata <90573281+addressata@users.noreply.github.com>
2021-09-28 21:29:37 +0300

    Add files via upload

---
+++ Лабараторная №1/Лабораторная №1.6/Лабораторная №1.6/Лабораторная №1.6.cpp
@@ -0,0 +1,13 @@
+﻿
+#include <iostream>
+
+int main()
+{
+    long long year_1, year_100, year_1000;
+    year_1 = 60 * 60 * 24 * 365;
+    year_100 = year_1 * 100;
+    year_1000 = year_100 * 10;
+
+}
+
+
